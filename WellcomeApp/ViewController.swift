d7f1945c6824042a033cc153998c9060995296ad
VK <art.nebar@mail.ru>
2022-11-16 13:32:11 +0300

    First View

--- WellcomeApp/ViewController.swift
+++ WellcomeApp/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    let realUserName = "Andrey"
+    let realPassword = "qwert"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func logInButtonAction() {
+        
+        if userNameTF.text == realUserName && passwordTF.text == realPassword {
+           
+            print("Имия и пароль совпадаеют")
+            
+        } else {
+            let title = "Invalid login or password"
+            let message = "Please, enter correct login and password"
+            
+            sendAlertMessage(title: title, message: message)
+        }
+    }
+    
+    @IBAction func forgotUserNameButtonAction() {
+        
+        let title = "Input correct User Name:"
+        let message = "\(realUserName)"
+        
+        sendAlertMessage(title: title, message: message)
+    }
+    
+    @IBAction func forgotPassButtonAction() {
+        
+        let title = "Input correct Password:"
+        let message = "\(realPassword)"
+        
+        sendAlertMessage(title: title, message: message)
+    }
+    
+    private func sendAlertMessage(title: String, message: String) {
+        
+        let alert = UIAlertController(
+            title: "\(title)",
+            message: "\(message)",
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = ""
+            self.userNameTF.text = ""
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
-
